ea2a71a6d14d6183c51a9693a91b284f7767cb22
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2022-10-28 00:30:31 +0000

    hngintern

--- stage1/views.py
+++ stage1/views.py
@@ -4,11 +4,12 @@ from django.http import JsonResponse
 
 
 def getinfo(request):
-    content = {
-        "slackUsername":"Johanan",
-        "backend":True,
-        "age":22,
-        "bio":"I like watching anime and working with computers"
-    }
-    return JsonResponse(content)
+    if request.method == "GET":
+        content = {
+            "slackUsername":"Johanan",
+            "backend":True,
+            "age":22,
+            "bio":"I like watching anime and working with computers"
+        }
+        return JsonResponse(content)
     
